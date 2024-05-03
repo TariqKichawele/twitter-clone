a78320a10db71157383a804b8c2531139a4e5855
TariqKichawele <tariqkichawele66@gmail.com>
2024-05-03 10:52:05 -0400

    UI design with dummy data

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from 'cloudinary';
+
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoute.js';
 import postRoutes from './routes/postRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
 import connectMongoDB from './db/connectMongoDB.js';
-import cookieParser from 'cookie-parser';
-import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config();
 
@@ -19,14 +20,10 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.json("Server is ready");
-})
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
 app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
